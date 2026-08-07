aeca2974c813da332e4d727cf0a3819a95acd17e
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2026-08-07 06:41:09 -0500

    win32 mouseinput struct for API

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+
+//https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-mouseinput
+type mouseInput struct {
+	dx, dy      int32  //LONG
+	mouseData   uint32 //DWORD
+	dwFlags     uint32
+	time        uint32
+	dwExtraInfo uintptr
+}
