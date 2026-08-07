91cf94541c6e91df7ecdcabc66bd25c987df055a
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2026-08-07 07:15:27 -0500

    these are Windows deets, oops, moved to Windows file. Too excited to get into it.

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
 package main
-
-//https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-mouseinput
-type MouseInput struct {
-	Dx, Dy      int32  //LONG
-	MouseData   uint32 //DWORD
-	DwFlags     uint32
-	Time        uint32
-	DwExtraInfo uintptr
-}
-
-//https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-input
-type Input struct {
-	Type uint32
-	Mi   MouseInput
-}
