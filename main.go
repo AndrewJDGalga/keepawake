487b1a5b4b2ed0e5e52f2503ec45131b3ade24cf
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2026-08-07 07:12:22 -0500

    seems I can recreate only the parts of input I need, though AI mentions this may be fragile?

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 //https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-mouseinput
-type mouseInput struct {
-	dx, dy      int32  //LONG
-	mouseData   uint32 //DWORD
-	dwFlags     uint32
-	time        uint32
-	dwExtraInfo uintptr
+type MouseInput struct {
+	Dx, Dy      int32  //LONG
+	MouseData   uint32 //DWORD
+	DwFlags     uint32
+	Time        uint32
+	DwExtraInfo uintptr
+}
+
+//https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-input
+type Input struct {
+	Type uint32
+	Mi   MouseInput
 }
