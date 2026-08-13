cfddad14c87fddc4f797088a0b4ea8764f3efbae
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2026-08-12 20:46:48 -0500

    os-signal pause-then-stop, electing for simplier idiom because I feel I understand it.

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/AndrewJDGalga/keep_awake/keepawake"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/AndrewJDGalga/keep_awake/keepawake"
+)
 
 func main() {
 	awake := keepawake.New()
 	if err := awake.Start(); err != nil {
-		panic(err)
+		log.Fatalf("Failed to start: %v\n", err)
 	}
 
+	log.Println("Keepawake is active.\nPress CTRL+C to quit.")
+
+	//capture interrupt and attempt graceful exit, especially on Windows
+	sChan := make(chan os.Signal, 1)
+	signal.Notify(sChan, os.Interrupt, syscall.SIGTERM)
+	<-sChan
+
+	log.Println("Attempting graceful quit...")
+	if err := awake.Stop(); err != nil {
+		log.Fatalf("Warning: Failed to release lock! Error: %v\n", err)
+	}
+	log.Println("Success!")
 }
